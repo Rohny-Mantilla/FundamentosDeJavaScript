38a54f2a6162f608ae4c498ef4f0856f06b80c17
Rohny <ecu_runy@hotmail.com>
2021-03-08 18:43:28 +0100

    Clase sobre comparaciones en JavaScript

---
+++ comparaciones.js
@@ -0,0 +1,21 @@
+var x = 4,
+  y = "4";
+
+var rohny = {
+  nombre: "rohny",
+};
+
+//false
+var otraPersona1 = {
+  nombre: "rohny",
+};
+
+//true
+var otraPersona2 = {
+  nombre: "rohny",
+};
+
+//false
+var otraPersona3 = {
+  ...rohny,
+};
