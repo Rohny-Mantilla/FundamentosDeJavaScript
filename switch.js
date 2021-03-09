3d8b66b64e32abd39fb761820aafeb6633f1d223
Rohny <ecu_runy@hotmail.com>
2021-03-09 14:41:27 +0100

    Clase de estructura repetitiva switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+var signo = prompt("Cual es tu signo?");
+
+switch (signo) {
+  case "leo":
+    console.log("hsdnasjndasndjsnjdnasjknsnadsjn");
+
+    break;
+  case "acuario":
+    console.log("hjdnasjknsnadsjn");
+
+    break;
+  default:
+    console.log("no es un signo");
+    break;
+}
