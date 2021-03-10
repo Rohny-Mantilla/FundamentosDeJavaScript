909a663181355419d6f26d5e443103ed35057315
Rohny <ecu_runy@hotmail.com>
2021-03-10 01:18:42 +0100

    Clase de reducir el valor de un array a un solo numero No lo entendi

--- filtrar_array.js
+++ filtrar_array.js
@@ -2,18 +2,21 @@ var rohny = {
   nombre: "Rohny",
   apellido: "Mantilla",
   altura: 1.63,
+  libros: 243,
 };
 
 var pamela = {
   nombre: "Pamela",
   apellido: "Rivera",
   altura: 1.83,
+  libros: 143,
 };
 
 var abigail = {
   nombre: "Abigail",
   apellido: "Rivera",
   altura: 1.58,
+  libros: 43,
 };
 
 const esAlta = ({ altura }) => altura > 1.8;
@@ -34,4 +37,15 @@ const pasarAlturaACms = (persona) => {
 
 var personasCms = personas.map(pasarAlturaACms);
 
-console.log(personasCms);
+// var acum = 0;
+
+// for (var i = 0; i < personas.length; i++) {
+//   acum = acum + personas[i].libros;
+// }
+
+//reduce reduce el valor de un array a un solo numero
+const reducer = (acum, { libros }) => acum + libros;
+
+var totalDeLibros = personas.reduce(reducer, 0);
+
+console.log(`En total todos tienen ${totalDeLibros} libros`);
