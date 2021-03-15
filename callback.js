35837121e871434cce6dcd44f013140a4c803bed
Rohny <ecu_runy@hotmail.com>
2021-03-15 12:44:32 +0100

    haciendo multiples requests

--- callback.js
+++ callback.js
@@ -5,7 +5,14 @@ const lukUrl = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`;
 const opts = { crossDomain: true };
 
 const onPeopleResponse = function (persona) {
-  console.log(`Hola yo soy, ${persona.name}`);
+  console.log(`Hola, yo soy ${persona.name}`);
 };
 
-$.get(lukUrl, opts, onPeopleResponse);
+function obtenerPersonaje(id) {
+  const Url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+  $.get(Url, opts, onPeopleResponse);
+}
+
+obtenerPersonaje(1);
+obtenerPersonaje(2);
+obtenerPersonaje(3);
