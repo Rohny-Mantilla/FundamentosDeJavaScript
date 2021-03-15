13c7dd7bbbd597c2043270e4a7465531d1b90758
Rohny <ecu_runy@hotmail.com>
2021-03-15 13:02:23 +0100

    manejando el orden de los requests y el asincronismo en JavaScript

--- callback.js
+++ callback.js
@@ -4,15 +4,19 @@ const PEOPLE_URL = "people/:id";
 const lukUrl = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`;
 const opts = { crossDomain: true };
 
-const onPeopleResponse = function (persona) {
-  console.log(`Hola, yo soy ${persona.name}`);
-};
-
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
   const Url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
-  $.get(Url, opts, onPeopleResponse);
+  $.get(Url, opts, function (persona) {
+    console.log(`Hola, yo soy ${persona.name}`);
+
+    if (callback) {
+      callback();
+    }
+  });
 }
 
-obtenerPersonaje(1);
-obtenerPersonaje(2);
-obtenerPersonaje(3);
+obtenerPersonaje(1, function () {
+  obtenerPersonaje(2, function () {
+    obtenerPersonaje(3);
+  });
+});
