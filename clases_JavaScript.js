f384a0ff4cb2786d974b5c3f4cc92be9fd892b76
Rohny <ecu_runy@hotmail.com>
2021-03-15 11:10:25 +0100

    Funciones como parametros de otra funcion. Muy interesante

--- clases_JavaScript.js
+++ clases_JavaScript.js
@@ -7,8 +7,12 @@ class persona {
     this.altura = altura;
   }
 
-  saludar() {
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+  saludar(fn) {
+    var { nombre, apellido } = this;
+    console.log(`Hola, me llamo ${nombre} ${apellido}`);
+    if (fn) {
+      fn(nombre, apellido, null);
+    }
   }
   soyAlto() {
     return this.altura > 1.8;
@@ -20,9 +24,24 @@ class Desarollador extends persona {
   constructor(nombre, apellido, altura) {
     super(nombre, apellido, altura);
   }
-  saludar() {
-    console.log(
-      `Hola me llamo ${this.nombre} ${this.apellido} y soy desarollador`
-    );
+  saludar(fn) {
+    var { nombre, apellido } = this;
+    console.log(`Hola me llamo ${nombre} ${apellido} y soy desarollador`);
+    if (fn) {
+      fn(nombre, apellido, true);
+    }
   }
 }
+
+function responderSaludo(nombre, apellido, esDev) {
+  console.log(`Buen dia ${nombre} ${apellido}`);
+  if (esDev) {
+    console.log(`Ah mira, no sabia que eras desarollador`);
+  }
+}
+
+var rohny = new Desarollador("rohny", "mantilla", 1.84);
+var abigail = new persona("abigail", "mantilla", 1.65);
+
+rohny.saludar(responderSaludo);
+abigail.saludar();
