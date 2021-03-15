a58b2e44ac369f3b7221d6bad9c8e5cd0b8ee462
Rohny <ecu_runy@hotmail.com>
2021-03-15 01:41:51 +0100

    Herencias mas simplificadas. Clase muy importante

--- clases_JavaScript.js
+++ clases_JavaScript.js
@@ -1,45 +1,28 @@
 //prototipo
-function heredaDe(protoHijo, protoPadre) {
-  var fn = function () {};
-  fn.prototype = protoPadre.prototype;
-  protoHijo.prototype = new fn();
-  protoHijo.prototype.constructor = protoHijo;
-}
 
-function persona(nombre, apellido, altura) {
-  this.nombre = nombre;
-  this.apellido = apellido;
-  this.altura = altura;
-}
-persona.prototype.saludar = function () {
-  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
-};
-
-persona.prototype.Altura = function () {
-  if (this.altura > 1.8) {
-    console.log(`${this.nombre} es alto`);
-  } else {
-    console.log(`${this.nombre} es bajo`);
+class persona {
+  constructor(nombre, apellido, altura) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.altura = altura;
   }
-};
 
-function Desarollador(nombre, apellido) {
-  this.nombre = nombre;
-  this.apellido = apellido;
+  saludar() {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+  }
+  soyAlto() {
+    return this.altura > 1.8;
+  }
 }
 
-heredaDe(Desarollador, persona);
-
-Desarollador.prototype.saludar = function () {
-  console.log(
-    `Hola me llamo ${this.nombre} ${this.apellido} y soy desarollador`
-  );
-};
-
-// var rohny = new persona("rohny", "mantilla", 1.84);
-// var abigail = new persona("abigail", "rivera", 1.63);
-
-// rohny.Altura();
-// abigail.Altura();
-
-//
+//heredar
+class Desarollador extends persona {
+  constructor(nombre, apellido, altura) {
+    super(nombre, apellido, altura);
+  }
+  saludar() {
+    console.log(
+      `Hola me llamo ${this.nombre} ${this.apellido} y soy desarollador`
+    );
+  }
+}
