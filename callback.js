7d0c9c364125572ce98e65ac3485db72270abd8c
Rohny <ecu_runy@hotmail.com>
2021-03-15 17:16:45 +0100

    Async-await UNa clase muy interesante sobre el asincronismo

--- callback.js
+++ callback.js
@@ -17,21 +17,39 @@ function onError(id) {
   console.log(`Susedio un error al obtener el personaje ${id}`);
 }
 
-obtenerPersonaje(1)
-  .then((personaje) => {
-    console.log(`El personaje 1 es ${personaje.name}`);
-    return obtenerPersonaje(2);
-  })
-  .then((personaje) => {
-    console.log(`El personaje 2 es ${personaje.name}`);
-    return obtenerPersonaje(3);
-  })
-  .then((personaje) => {
-    console.log(`El personaje 3 es ${personaje.name}`);
-    return obtenerPersonaje(4);
-  })
-  .then((personaje) => {
-    console.log(`El personaje 4 es ${personaje.name}`);
-    return obtenerPersonaje(5);
-  })
-  .catch(onError);
+// var ids = [1, 2, 3, 4, 5, 6, 7]
+// var promesas = ids.map(function(id) {
+//   return obtenerPersonaje(id)
+// })
+
+async function obtenerPersonajes() {
+  var ids = [1, 2, 3, 4, 5, 6, 7];
+  var promesas = ids.map((id) => obtenerPersonaje(id));
+  try {
+    var personajes = await Promise.all(promesas);
+    console.table(personajes);
+  } catch (id) {
+    onError(id);
+  }
+}
+
+obtenerPersonajes();
+
+// obtenerPersonaje(1)
+//   .then((personaje) => {
+//     console.log(`El personaje 1 es ${personaje.name}`);
+//     return obtenerPersonaje(2);
+//   })
+//   .then((personaje) => {
+//     console.log(`El personaje 2 es ${personaje.name}`);
+//     return obtenerPersonaje(3);
+//   })
+//   .then((personaje) => {
+//     console.log(`El personaje 3 es ${personaje.name}`);
+//     return obtenerPersonaje(4);
+//   })
+//   .then((personaje) => {
+//     console.log(`El personaje 4 es ${personaje.name}`);
+//     return obtenerPersonaje(5);
+//   })
+//   .catch(onError);
